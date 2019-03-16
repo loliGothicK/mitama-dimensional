a61ea65f7ea9b3f6ab4358c10f473c7bce8f2b1e
mitama <loligothicl@gmail.com>
2019-03-16 19:29:56 +0900

    add: scaled molar quantifiers

--- include/dimensional/si/mol.hpp
+++ include/dimensional/si/mol.hpp
@@ -14,7 +14,13 @@ using micromol_t = scaled_unit_t<mol_t, std::micro>;
 using kilomol_t = scaled_unit_t<mol_t, std::kilo>;
 
 inline constexpr mol_t mols{};
+inline constexpr millimol_t millimols{};
+inline constexpr micromol_t micromols{};
+inline constexpr kilomol_t kilomols{};
 
 template < std::intmax_t N = 1 > inline constexpr powered_t<mol_t, N> mol{};
+template < std::intmax_t N = 1 > inline constexpr powered_t<millimol_t, N> millimol{};
+template < std::intmax_t N = 1 > inline constexpr powered_t<micromol_t, N> micromol{};
+template < std::intmax_t N = 1 > inline constexpr powered_t<kilomol_t, N> kilomols{};
 
 }
