b0e9b1064a9f0d555a33907e581361efa3f7f425
mitama <loligothick@gmail.com>
2019-03-18 19:27:07 +0900

    fix: move class delta to new header delta.hpp

---
+++ include/dimensional/delta.hpp
@@ -0,0 +1,117 @@
+#ifndef MITAMA_DIMENSIONAL_DELTA_HPP
+#define MITAMA_DIMENSIONAL_DELTA_HPP
+#include "quantity.hpp"
+#include "derived_units/dgree_celsius.hpp"
+#include "si/kelvin.hpp"
+
+namespace mitama {
+
+template < class > struct is_temperature_quantity: std::false_type {};
+template < class T > struct is_temperature_quantity<quantity_t<si::kelvin_t, T>>: std::true_type {};
+template < class T > struct is_temperature_quantity<quantity_t<nonsi::dgree_celsius_t, T>>: std::true_type {};
+
+template < class T > inline constexpr bool is_temperature_quantity_v = is_temperature_quantity<T>::value;
+
+template <class,class=void>
+class delta;
+
+template < class T >
+class delta<T, std::enable_if_t<is_quantity_v<T>>> {
+  T value_;
+public:
+  using value_type = T;
+  delta() = delete;
+
+  template <class U = T,
+            std::enable_if_t<
+                  std::is_constructible_v<T, U> &&
+                  !std::is_convertible_v<U, T>
+            , bool> = false>
+  constexpr explicit delta(U && u) noexcept(std::is_nothrow_constructible_v<T, U>)
+      : value_(std::forward<U>(u)) {}
+
+  template <typename U = T,
+            std::enable_if_t<
+                  std::is_constructible_v<T, U> &&
+                  std::is_convertible_v<U, T>
+            , bool> = false>
+  constexpr delta(U && u) noexcept(std::is_nothrow_constructible_v<T, U>)
+      : value_(std::forward<U>(u)) {}
+
+  T get() const { return value_; }
+
+  friend std::ostream& operator<<(std::ostream& os, delta const& d) {
+    return os << d.value_;
+  }
+};
+
+template <class T> delta(T&&) -> delta<std::decay_t<T>>;
+
+
+template < class T, class Quantity,
+          std::enable_if_t<!is_temperature_quantity_v<T> || !is_temperature_quantity_v<std::decay_t<Quantity>>, bool> = false,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() + std::declval<Quantity>())>>, bool> = false>
+Quantity operator+(delta<T> const& d, Quantity const& q){
+  return q + Quantity(d.get());
+}
+template < class T, class Quantity,
+          std::enable_if_t<!is_temperature_quantity_v<T> || !is_temperature_quantity_v<std::decay_t<Quantity>>, bool> = false,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() + std::declval<Quantity>())>>, bool> = false>
+Quantity operator+(Quantity const& q, delta<T> const& d){
+  return q + Quantity(d.get());
+}
+template < class T, class U,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() + std::declval<U>())>>, bool> = false>
+auto operator+(delta<T> const& d1, delta<U> const d2){
+  return d1.get() + d2.get();
+}
+template < class T, class Quantity,
+          std::enable_if_t<!is_temperature_quantity_v<T> || !is_temperature_quantity_v<std::decay_t<Quantity>>, bool> = false,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() - std::declval<Quantity>())>>, bool> = false>
+Quantity operator-(delta<T> d, Quantity const& q){
+  return q - Quantity(d.get());
+}
+template < class T, class Quantity,
+          std::enable_if_t<!is_temperature_quantity_v<T> || !is_temperature_quantity_v<std::decay_t<Quantity>>, bool> = false,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() - std::declval<Quantity>())>>, bool> = false>
+Quantity operator-(Quantity const& q, delta<T> d){
+  return q - Quantity(d.get());
+}
+template < class T, class U,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() - std::declval<U>())>>, bool> = false>
+auto operator-(delta<T> d1, delta<U> d2){
+  return d1.get() - d2.get();
+}
+template < class T, class Quantity,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() * std::declval<Quantity>())>>, bool> = false>
+Quantity operator*(delta<T> d, Quantity const& q){
+  return q * Quantity(d.get());
+}
+template < class T, class Quantity,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() * std::declval<Quantity>())>>, bool> = false>
+Quantity operator*(Quantity const& q, delta<T> d){
+  return q * Quantity(d.get());
+}
+template < class T, class U,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() * std::declval<U>())>>, bool> = false>
+auto operator*(delta<T> d1, delta<U> d2){
+  return d1.get() * d2.get();
+}
+template < class T, class Quantity,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() / std::declval<Quantity>())>>, bool> = false>
+Quantity operator/(delta<T> d, Quantity const& q){
+  return q / Quantity(d.get());
+}
+template < class T, class Quantity,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() / std::declval<Quantity>())>>, bool> = false>
+Quantity operator/(Quantity const& q, delta<T> d){
+  return q / Quantity(d.get());
+}
+template < class T, class U,
+          std::enable_if_t<is_quantity_v<std::decay_t<decltype(std::declval<T>() / std::declval<U>())>>, bool> = false>
+auto operator/(delta<T> d1, delta<U> d2){
+  return d1.get() / d2.get();
+}
+}
+
+#endif
