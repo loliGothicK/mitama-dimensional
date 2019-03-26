813141305fe81c1aa5e0fb1473f2c62771dfffc7
mitama <loligothick@gmail.com>
2019-03-26 22:20:32 +0900

    feat: math func dim

--- include/dimensional/math/minmax.hpp
+++ include/dimensional/math/minmax.hpp
@@ -7,7 +7,9 @@
 namespace mitama {
 
 template <class First, class Second, class... Quantities>
-auto min(First first, Second second, Quantities... quantities) {
+auto min(First first, Second second, Quantities... quantities)
+  -> ::mitama::common_type_t<First, Second, Quantities...>
+{
   using ret = ::mitama::common_type_t<First, Second, Quantities...>;
   if constexpr (sizeof...(Quantities) == 0) {
     return first < second ? static_cast<ret>(first) : static_cast<ret>(second);
@@ -19,7 +21,9 @@ auto min(First first, Second second, Quantities... quantities) {
 }
 
 template <class First, class Second, class... Quantities>
-auto max(First first, Second second, Quantities... quantities) {
+auto max(First first, Second second, Quantities... quantities)
+  -> ::mitama::common_type_t<First, Second, Quantities...>
+{
   using ret = ::mitama::common_type_t<First, Second, Quantities...>;
   if constexpr (sizeof...(Quantities) == 0) {
     return first > second ? static_cast<ret>(first) : static_cast<ret>(second);
@@ -28,6 +32,12 @@ auto max(First first, Second second, Quantities... quantities) {
                                         : static_cast<ret>(second),
                          quantities...);
   }
+}
+
+template < class Q1, class Q2 >
+auto dim(Q1&& q1, Q2&& q2) -> ::mitama::common_type_t<Q1, Q2>
+{
+  return ::mitama::common_type_t<Q1, Q2>( std::fdim(std::forward<Q1>(q1).get(), std::forward<Q2>(q2).get()) );
 }
 
 } // namespace mitama
