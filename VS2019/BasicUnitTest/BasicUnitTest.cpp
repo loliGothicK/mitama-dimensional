775153e3fea8c77c3d1ab81206ee6e1225ba8605
Yuki Wada <loligothick@gmail.com>
2019-04-04 07:23:41 +0900

    Visual Studio 2019 launch!

---
+++ VS2019/BasicUnitTest/BasicUnitTest.cpp
@@ -0,0 +1,97 @@
+// main is provided in tests-main.cpp
+#include "pch.h"
+#include <catch2/catch.hpp>
+#include <dimensional/quantity.hpp>
+#include <dimensional/si_units/all.hpp>
+#include <dimensional/prefix.hpp>
+#include <dimensional/arithmetic.hpp>
+#include <dimensional/refinement.hpp>
+#include <dimensional/nonsi_units/minute.hpp>
+#include <dimensional/nonsi_units/hour.hpp>
+#include <dimensional/nonsi_units/day.hpp>
+#include <test_util.hpp>
+
+using namespace mitama;
+using namespace mitama::si;
+
+namespace BasicUnitTest
+{
+
+TEMPLATE_TEST_CASE("constructor from values",
+	"[quantity][constructor]",
+	meter_t, ampere_t, candela_t, kelvin_t, kilogram_t, mol_t, second_t)
+{
+	quantity<TestType, int> a(1);
+	REQUIRE(a.get() == 1);
+
+	quantity<scaled_unit_t<TestType, std::milli>, double> b = a;
+	REQUIRE(b.get() == 1000);
+}
+
+TEMPLATE_TEST_CASE("meter_t from other quantities",
+	"[quantity][constructor]",
+	ampere_t, candela_t, kelvin_t, kilogram_t, mol_t, second_t)
+{
+	REQUIRE(!std::is_constructible_v<meter_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("ampere_t from other quantities",
+	"[quantity][constructor]",
+	meter_t, candela_t, kelvin_t, kilogram_t, mol_t, second_t)
+{
+	REQUIRE(!std::is_constructible_v<ampere_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("candela_t from other quantities",
+	"[quantity][constructor]",
+	meter_t, ampere_t, kelvin_t, kilogram_t, mol_t, second_t)
+{
+	REQUIRE(!std::is_constructible_v<candela_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("kelvin_t from other quantities",
+	"[quantity][constructor]",
+	meter_t, ampere_t, candela_t, kilogram_t, mol_t, second_t)
+{
+	REQUIRE(!std::is_constructible_v<kelvin_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("kilogram_t from other quantities",
+	"[quantity][constructor]",
+	meter_t, ampere_t, candela_t, kelvin_t, mol_t, second_t)
+{
+	REQUIRE(!std::is_constructible_v<kilogram_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("mol_t from other quantities",
+	"[quantity][constructor]",
+	meter_t, ampere_t, candela_t, kelvin_t, kilogram_t, second_t)
+{
+	REQUIRE(!std::is_constructible_v<mol_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("second_t from other quantities",
+	"[quantity][constructor]",
+	meter_t, ampere_t, candela_t, kelvin_t, kilogram_t, mol_t)
+{
+	REQUIRE(!std::is_constructible_v<second_t, TestType>);
+}
+
+TEMPLATE_TEST_CASE("validates",
+	"[quantity][validate]",
+	second_t, meter_t, ampere_t, candela_t, kelvin_t, kilogram_t, mol_t)
+{
+	auto validator = [](auto v) -> mitama::Result<quantity<TestType, int>, int> {
+		if (v.get() < 0)
+			return mitama::Err(v.get());
+		else
+			return mitama::Ok(v);
+	};
+
+	REQUIRE((1 | TestType{}).validate(validator) == Ok(1 | TestType{}));
+	REQUIRE((-1 | TestType{}).validate(validator) == Err(-1));
+	auto v = refined_for<sym::L<1>>(1 | meters);
+}
+
+
+}
