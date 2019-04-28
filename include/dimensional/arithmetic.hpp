9d8e57c4fee391dda7824cb5363dfa6ca57445e0
mitama <loligothicl@gmail.com>
2019-04-28 09:22:43 +0900

    fix: operator* and operator/ for quantity

--- include/dimensional/arithmetic.hpp
+++ include/dimensional/arithmetic.hpp
@@ -36,60 +36,59 @@ operator-(L &&lhs, R &&rhs)
 
 template <class L, class R,
           std::enable_if_t<
-            std::disjunction_v<is_quantity<std::decay_t<L>>, is_quantity<std::decay_t<R>>> &&
-            !is_expr_v<std::decay_t<L>> && !is_expr_v<std::decay_t<R>> &&
-            !is_delta_v<std::decay_t<L>> && !is_delta_v<std::decay_t<R>>
+            std::conjunction_v<is_quantity<std::decay_t<L>>, is_quantity<std::decay_t<R>>>
             , bool> = false>
 constexpr auto
 operator*(L &&lhs, R &&rhs)
 {
-    if constexpr (is_quantity_v<std::decay_t<L>> && is_quantity_v<std::decay_t<R>>) {
-        return quantity_t<
-                mitamagic::quotient_t<typename std::decay_t<L>::dimension_type,
-                                      typename std::decay_t<R>::dimension_type
-                >,
-                std::common_type_t<typename std::decay_t<L>::value_type,
-                                   typename std::decay_t<R>::value_type>>
-            {
-                mitamagic::scaled_value(std::forward<L>(lhs), std::forward<R>(rhs), [](auto a, auto b){ return a * b; })
-            };
-    }
-    else if constexpr (is_quantity_v<std::decay_t<L>>) {
-        return std::decay_t<L>(lhs.get() * rhs);
-    }
-    else {
-        return std::decay_t<L>(rhs.get() * lhs);
-    }
+    return quantity_t<
+            mitamagic::quotient_t<typename std::decay_t<L>::dimension_type,
+                                    typename std::decay_t<R>::dimension_type
+            >,
+            std::common_type_t<typename std::decay_t<L>::value_type,
+                                typename std::decay_t<R>::value_type>>
+        {
+            mitamagic::scaled_value(std::forward<L>(lhs), std::forward<R>(rhs), [](auto a, auto b){ return a * b; })
+        };
+}
+
+template <class L,
+          std::enable_if_t<is_quantity_v<std::decay_t<L>>, bool> = false>
+constexpr auto
+operator*(L &&lhs, typename std::decay_t<L>::value_type const& rhs)
+{
+    return std::decay_t<L>(std::forward<L>(lhs).get() * rhs);
+}
+
+template <class R,
+          std::enable_if_t<is_quantity_v<std::decay_t<R>>, bool> = false>
+constexpr auto
+operator*(typename std::decay_t<R>::value_type const& lhs, R&& rhs)
+{
+    return std::decay_t<R>(std::forward<R>(rhs).get() * lhs);
 }
 
 template <class L, class R,
           std::enable_if_t<
-            is_quantity_v<std::decay_t<L>> &&
-            !is_expr_v<std::decay_t<L>> && !is_expr_v<std::decay_t<R>> &&
-            !is_delta_v<std::decay_t<L>> && !is_delta_v<std::decay_t<R>>
+            std::conjunction_v<is_quantity<std::decay_t<L>>, is_quantity<std::decay_t<R>>>
             , bool> = false>
 constexpr auto
 operator/(L &&lhs, R &&rhs)
 {
-    if constexpr (is_quantity_v<std::decay_t<L>> && is_quantity_v<std::decay_t<R>>) {
-        return quantity_t<
-                mitamagic::quotient_t<typename std::decay_t<L>::dimension_type, mitamagic::inverse_t<typename std::decay_t<R>::dimension_type>>,
-                std::common_type_t<typename std::decay_t<L>::value_type, typename std::decay_t<R>::value_type>
-            >{
-                mitamagic::scaled_value(std::forward<L>(lhs), std::forward<R>(rhs), [](auto a, auto b){ return a / b; })
-            };
-    }
-    else if constexpr (is_quantity_v<std::decay_t<R>>) {
-        return quantity<
-                mitamagic::inverse_t<typename std::decay_t<R>::dimension_type>, // dimension type
-                std::common_type_t<typename std::decay_t<R>::value_type, std::decay_t<L>> // common value_type
-            >{
-                lhs / rhs.get()
-            };
-    }
-    else {
-        return std::decay_t<L>(lhs.get() / rhs);
-    }
+    return quantity_t<
+            mitamagic::quotient_t<typename std::decay_t<L>::dimension_type, mitamagic::inverse_t<typename std::decay_t<R>::dimension_type>>,
+            std::common_type_t<typename std::decay_t<L>::value_type, typename std::decay_t<R>::value_type>
+        >{
+            mitamagic::scaled_value(std::forward<L>(lhs), std::forward<R>(rhs), [](auto a, auto b){ return a / b; })
+        };
+}
+
+template <class L,
+          std::enable_if_t<is_quantity_v<std::decay_t<L>>, bool> = false>
+constexpr auto
+operator/(L &&lhs, typename std::decay_t<L>::value_type const& rhs)
+{
+    return std::decay_t<L>(std::forward<L>(lhs).get() / rhs);
 }
 
 }
