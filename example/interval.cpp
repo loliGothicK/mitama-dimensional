9a387a9d346831cf08cb4339cf71815179dbd5f0
mitama <loligothicl@gmail.com>
2019-05-15 07:08:44 +0900

    fix: examples

--- example/interval.cpp
+++ example/interval.cpp
@@ -1,12 +1,12 @@
 #include "../include/dimensional/quantity.hpp"
 #include "../include/dimensional/arithmetic.hpp"
-#include "../include/dimensional/si_units/all.hpp"
+#include "../include/dimensional/systems/si/all.hpp"
 #include "../include/dimensional/io.hpp"
 #include "../cranberries/interval.hpp"
 
 int main() {
     using namespace mitama;
-    namespace si = mitama::si;
+    namespace si = mitama::systems::si;
     using cranberries::interval;
 
     interval<double> a (1, 2);
