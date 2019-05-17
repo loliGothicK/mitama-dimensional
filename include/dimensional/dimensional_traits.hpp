3acb52e1f2bb8924b1d00e10bae2ece0185b9ab7
mitama <loligothicl@gmail.com>
2019-05-17 12:49:25 +0900

    refactor: include/dimensional/dimensional_traits.hpp

--- include/dimensional/dimensional_traits.hpp
+++ include/dimensional/dimensional_traits.hpp
@@ -15,6 +15,15 @@ namespace mitama {
     template <class D, class T>
     struct is_quantity<quantity_t<D, T>> : std::true_type {};
 
+    template <class D, class T>
+    struct is_quantity<const quantity_t<D, T>> : std::true_type {};
+
+    template <class D, class T>
+    struct is_quantity<volatile quantity_t<D, T>> : std::true_type {};
+
+    template <class D, class T>
+    struct is_quantity<const volatile quantity_t<D, T>> : std::true_type {};
+
     template <class T> inline constexpr bool is_quantity_v = is_quantity<T>::value;
 
     // units_t traits
@@ -22,6 +31,15 @@ namespace mitama {
 
     template <class D, class E, class S>
     struct is_units<units_t<D, E, S>> : std::true_type {};
+
+    template <class D, class E, class S>
+    struct is_units<const units_t<D, E, S>> : std::true_type {};
+
+    template <class D, class E, class S>
+    struct is_units<volatile units_t<D, E, S>> : std::true_type {};
+
+    template <class D, class E, class S>
+    struct is_units<const volatile units_t<D, E, S>> : std::true_type {};
 
     template <class U> inline constexpr bool is_units_v = is_units<U>::value;
 
@@ -31,23 +49,28 @@ namespace mitama {
     template <template <class> class Synonym, class... Units>
     struct is_dimensional<Synonym<dimensional_t<Units...>>> : std::true_type {};
 
+    template <template <class> class Synonym, class... Units>
+    struct is_dimensional<const Synonym<dimensional_t<Units...>>> : std::true_type {};
+
+    template <template <class> class Synonym, class... Units>
+    struct is_dimensional<volatile Synonym<dimensional_t<Units...>>> : std::true_type {};
+
+    template <template <class> class Synonym, class... Units>
+    struct is_dimensional<const volatile Synonym<dimensional_t<Units...>>> : std::true_type {};
+
     template <class D>
     inline constexpr bool is_dimensional_v = is_dimensional<D>::value;
 
-    // quantity_t traits
-    // meta-operator for dimension equivalence
-    // primary template
-    template <class, class> struct is_same_dimensional : std::false_type {};
+namespace mitamagic {
+    template < class, class > struct is_same_dimensional_impl: std::false_type {};
 
-    // meta-operator for dimension equivalence
-    // template partial specialization for dimensional_t
     template <class T, class U,
               class... Units1,
               class... Units2,
               template <class> class Synonym1,
               template <class> class Synonym2>
-    struct is_same_dimensional<quantity_t<Synonym1<dimensional_t<Units1...>>, T>,
-                               quantity_t<Synonym2<dimensional_t<Units2...>>, U>>
+    struct is_same_dimensional_impl<quantity_t<Synonym1<dimensional_t<Units1...>>, T>,
+                                    quantity_t<Synonym2<dimensional_t<Units2...>>, U>>
         : std::conjunction<
             std::bool_constant<sizeof...(Units1) == sizeof...(Units2)>,
             std::is_base_of<typename Units1::tag,
@@ -57,8 +80,8 @@ namespace mitama {
               class... Units2,
               template <class> class Synonym1,
               template <class> class Synonym2>
-    struct is_same_dimensional<Synonym1<dimensional_t<Units1...>>,
-                               Synonym2<dimensional_t<Units2...>>>
+    struct is_same_dimensional_impl<Synonym1<dimensional_t<Units1...>>,
+                                    Synonym2<dimensional_t<Units2...>>>
         : std::conjunction<
             std::bool_constant<sizeof...(Units1) == sizeof...(Units2)>,
             std::is_base_of<typename Units1::tag,
@@ -66,33 +89,48 @@ namespace mitama {
 
     template <class... Units1,
               class... Units2>
-    struct is_same_dimensional<dimensional_t<Units1...>,
-                               dimensional_t<Units2...>>
+    struct is_same_dimensional_impl<dimensional_t<Units1...>,
+                                    dimensional_t<Units2...>>
         : std::conjunction<
             std::bool_constant<sizeof...(Units1) == sizeof...(Units2)>,
             std::is_base_of<typename Units1::tag,
                             dimensional_t<Units2...>>...> {};
+}
 
+    template <class T, class U> using is_same_dimensional = mitamagic::is_same_dimensional_impl<std::decay_t<T>, std::decay_t<U>>;
 
     // alias variable template
     template <class L, class R>
     inline constexpr bool is_same_dimensional_v = is_same_dimensional<L, R>::value;
 
+namespace mitamagic {
     template < class, class = void >
     struct has_dimensionless_tag_impl: std::false_type {};
 
     template < class Dim >
-    struct has_dimensionless_tag_impl<Dim, typename Dim::dimension_type::is_dimensionless>: std::true_type {};
-
+    struct has_dimensionless_tag_impl<Dim, typename std::decay_t<Dim>::dimension_type::is_dimensionless>: std::true_type {};
+}
     template < class Dim >
-    struct has_dimensionless_tag: has_dimensionless_tag_impl<Dim> {};
+    struct has_dimensionless_tag: mitamagic::has_dimensionless_tag_impl<Dim> {};
 
     template < template <class> class, class >
     struct remove_dim_if;
 
     template < template <class> class Pred, template <class> class Synonym, class T, class... Units >
     struct remove_dim_if<Pred, quantity_t<Synonym<dimensional_t<Units...>>, T>> {
-    using type = quantity_t<Synonym<mitamagic::tlist_remove_if_t<Pred, dimensional_t<Units...>>>, T>;
+        using type = quantity_t<Synonym<mitamagic::tlist_remove_if_t<Pred, dimensional_t<Units...>>>, T>;
+    };
+    template < template <class> class Pred, template <class> class Synonym, class T, class... Units >
+    struct remove_dim_if<Pred, const quantity_t<Synonym<dimensional_t<Units...>>, T>> {
+        using type = const quantity_t<Synonym<mitamagic::tlist_remove_if_t<Pred, dimensional_t<Units...>>>, T>;
+    };
+    template < template <class> class Pred, template <class> class Synonym, class T, class... Units >
+    struct remove_dim_if<Pred, volatile quantity_t<Synonym<dimensional_t<Units...>>, T>> {
+        using type = volatile quantity_t<Synonym<mitamagic::tlist_remove_if_t<Pred, dimensional_t<Units...>>>, T>;
+    };
+    template < template <class> class Pred, template <class> class Synonym, class T, class... Units >
+    struct remove_dim_if<Pred, const volatile quantity_t<Synonym<dimensional_t<Units...>>, T>> {
+        using type = const volatile quantity_t<Synonym<mitamagic::tlist_remove_if_t<Pred, dimensional_t<Units...>>>, T>;
     };
 
     template < template <class> class Pred, class Q >
@@ -101,8 +139,8 @@ namespace mitama {
     template <class From, class To>
     struct is_dimensional_convertible
         : std::conjunction<
-            is_complete_type<converter<From, To>>,
-            is_same_dimensional<From, To>>
+            is_complete_type<converter<std::decay_t<From>, std::decay_t<To>>>,
+            is_same_dimensional<std::decay_t<From>, std::decay_t<To>>>
     {};
 
     template < class From, class To >
@@ -112,28 +150,37 @@ namespace mitama {
 
     template < template<class>class Synonym, class... Units >
     struct is_dimensional_quantifier<Synonym<dimensional_t<Units...>>>: std::true_type {};
+    template < template<class>class Synonym, class... Units >
+    struct is_dimensional_quantifier<const Synonym<dimensional_t<Units...>>>: std::true_type {};
+    template < template<class>class Synonym, class... Units >
+    struct is_dimensional_quantifier<volatile Synonym<dimensional_t<Units...>>>: std::true_type {};
+    template < template<class>class Synonym, class... Units >
+    struct is_dimensional_quantifier<const volatile Synonym<dimensional_t<Units...>>>: std::true_type {};
+
+    template < class T >
+    inline constexpr bool is_dimensional_quantifier_v = is_dimensional_quantifier<T>::value;
 
     template <class Q1, class Q2, class... Quantities>
     struct common_quantity
         : ::mitama::common_quantity<
-            quantity_t<mitamagic::scaled_dimension_t<typename Q1::dimension_type,
-                                                     typename Q2::dimension_type>,
-                        std::common_type_t<typename Q1::value_type,
-                                           typename Q2::value_type>>,
+            quantity_t<mitamagic::scaled_dimension_t<typename std::decay_t<Q1>::dimension_type,
+                                                     typename std::decay_t<Q2>::dimension_type>,
+                        std::common_type_t<typename std::decay_t<Q1>::value_type,
+                                           typename std::decay_t<Q2>::value_type>>,
             Quantities...>
     {
         static_assert(std::conjunction_v<is_same_dimensional<Q1, Q2>,
-                                        is_same_dimensional<Q1, Quantities>...>,
+                                         is_same_dimensional<Q1, Quantities>...>,
                         "dimension type refinement error: "
                         "common_type requires same dimension basis");
     };
 
     template <class Q1, class Q2> struct common_quantity<Q1, Q2> {
     using type = quantity_t<
-        mitamagic::scaled_dimension_t<typename Q1::dimension_type,
-                                      typename Q2::dimension_type>,
-        std::common_type_t<typename Q1::value_type,
-                           typename Q2::value_type>>;
+        mitamagic::scaled_dimension_t<typename std::decay_t<Q1>::dimension_type,
+                                      typename std::decay_t<Q2>::dimension_type>,
+        std::common_type_t<typename std::decay_t<Q1>::value_type,
+                           typename std::decay_t<Q2>::value_type>>;
     };
 
     template <class... Quantities>
@@ -141,7 +188,7 @@ namespace mitama {
 
     template <class U, std::intmax_t N>
     using powered_t =
-        typename mitamagic::powered_dimensional<U, std::ratio<N>>::type;
+        typename mitamagic::powered_dimensional<std::decay_t<U>, std::ratio<N>>::type;
 
     template < class U >
     using reciprocal_t = powered_t<U, -1>;
