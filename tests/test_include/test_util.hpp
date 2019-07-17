d91bb80aed0ccd60b2194401f42df77661319f74
Mitama <loligothick@gmail.com>
2019-07-18 03:36:11 +0900

    update submodules

--- tests/test_include/test_util.hpp
+++ tests/test_include/test_util.hpp
@@ -5,7 +5,7 @@
 #include <utility>
 #include <tuple>
 #include <random>
-#include <mitama/result/Result.hpp>
+#include <mitama/result/result.hpp>
 
 #define IS_INVALID_EXPR(...)                                     \
   IS_INVALID_EXPR_IMPL1(__VA_ARGS__)                             \
